088bd949b3b75a862ff08a1148f2e3aef3b9c206
Katelyn <katelynwest225@gmail.com>
2021-09-22 16:58:27 -0500

    Started setting up Run, Update, Start and End Methods.

---
+++ MonsterFighter/Game.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MonsterFighter
+{
+    class Game
+    {
+        public void Run()
+        {
+
+        }
+
+        void Start()
+        { 
+        }
+
+        void Update()
+        { 
+        }
+
+        void End()
+        {
+            
+        }
+
+
+    }
+}
