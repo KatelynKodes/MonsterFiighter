b41042a367e684fe2cbf1cc3186234d139f412e4
Katelyn <katelynwest225@gmail.com>
2021-09-24 11:55:39 -0500

    Created Player Class

---
+++ MonsterFighter/Player.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MonsterFighter
+{
+    class Player
+    {
+        private string _name;
+        private Monster[] _team;
+        private int _monsterTeamLength;
+
+        public Player()
+        {
+            _name = "Priest";
+            _team = new Monster[0];
+            _monsterTeamLength = _team.Length;
+        }
+
+        public void AddToTeam( Monster MonsterToAdd)
+        {
+            if (_team.Length != 0)
+            {
+                Monster[] newTeam = new Monster[_team.Length + 1];
+                for (int i = 0; i < _team.Length; i++)
+                {
+                    newTeam[i] = _team[i];
+                }
+            }
+            else
+            {
+                _team[0] = MonsterToAdd;
+            }
+        }
+
+    }
+}
+
